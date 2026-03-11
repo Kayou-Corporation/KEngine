acfeaebf123bfe7265e9cec6315adf9e4444d209
Corentin Bois Cotte <cboiscotte@persistant.fr>
2026-03-11 12:53:57 +0100

    [WIP] Change project architecture, cmake generates correctly, need to make everything work in cpp now

--- Engine/Code/Core/Include/Utils/Core.hpp
+++ Engine/Code/Core/Include/Utils/Core.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "KEngine.hpp"
+#define KAYOU_GLOBAL Kayou
+
+#define BEGIN_NAMESPACE_KAYOU namespace KAYOU_GLOBAL {
+#define END_NAMESPACE_KAYOU } /* namespace KAYOU_GLOBAL */
 
 #define CORE_NAMESPACE Core
 
--- Engine/Code/Include/KEngine.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define KAYOU_GLOBAL Kayou
-
-#define BEGIN_NAMESPACE_KAYOU namespace KAYOU_GLOBAL {
-#define END_NAMESPACE_KAYOU } /* namespace KAYOU_GLOBAL */
