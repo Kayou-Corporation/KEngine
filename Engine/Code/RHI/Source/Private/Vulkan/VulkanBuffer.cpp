275f3a2d0c395b84ebbea0716b86d4334788c843
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2026-03-11 20:36:24 +0100

    [ADD] TODO comment for statging buffer data copy

--- Engine/Code/RHI/Source/Private/Vulkan/VulkanBuffer.cpp
+++ Engine/Code/RHI/Source/Private/Vulkan/VulkanBuffer.cpp
@@ -4,6 +4,8 @@ BEGIN_NAMESPACE_RHI
 
 void VulkanBuffer::SetData(void* data, uint32_t size)
 {
+	// If the buffer is GPU only, we can't map it, so we need to use a staging buffer to copy the data to the GPU
+	// TODO: Implement staging buffer copy with single time command buffer
 	ASSERT(m_isGpuOnly, "Buffer is GPU only");
 	ASSERT(size > m_size, "Data is too large");
 
