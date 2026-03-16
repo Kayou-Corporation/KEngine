8fb3db46e2f99a796b0c347c76636b7798e2acbb
Corentin Bois Cotte <cboiscotte@persistant.fr>
2026-03-16 14:29:27 +0100

    [UPDATE] Adapted disable warnings macro so VMA doesn't trigger any warnings anymore with clang

--- Engine/Code/Core/Include/Utils/Core.hpp
+++ Engine/Code/Core/Include/Utils/Core.hpp
@@ -10,18 +10,20 @@
 #define BEGIN_NAMESPACE_CORE BEGIN_NAMESPACE_KAYOU namespace CORE_NAMESPACE {
 #define END_NAMESPACE_CORE } /* namespace CORE_NAMESPACE */ END_NAMESPACE_KAYOU
 
-#if defined(_MSC_VER)
+#if defined(__GNUC__) || defined(__clang__)
+#define DO_PRAGMA(x) _Pragma(#x)
+// Some warning flags might need to be added, see the flag in the output to add it here
+#define DISABLE_ALL_WARNINGS \
+        DO_PRAGMA(clang diagnostic push) \
+        DO_PRAGMA(clang diagnostic ignored "-Wall") \
+        DO_PRAGMA(clang diagnostic ignored "-Wextra") \
+        DO_PRAGMA(clang diagnostic ignored "-Wpedantic") \
+        DO_PRAGMA(clang diagnostic ignored "-Wnullability-completeness")
+#define RESTORE_WARNINGS DO_PRAGMA(clang diagnostic pop)
+#elif defined(_MSC_VER)
 // It is not possible to disable all warnings for MSVC, so if the macro doesn't ignore a specific warning you want ignored, feel free to add the warning code below
 #define DISABLE_ALL_WARNINGS __pragma(warning(push)) __pragma(warning(disable: 4100 4189 4244 4267 4456 4700 4701 4703 4996 4324))
 #define RESTORE_WARNINGS     __pragma(warning(pop))
-#elif defined(__GNUC__) || defined(__clang__)
-#define DO_PRAGMA(x) _Pragma(#x)
-#define DISABLE_ALL_WARNINGS \
-        DO_PRAGMA(GCC diagnostic push) \
-        DO_PRAGMA(GCC diagnostic ignored "-Wall") \
-        DO_PRAGMA(GCC diagnostic ignored "-Wextra") \
-        DO_PRAGMA(GCC diagnostic ignored "-Wpedantic")
-#define RESTORE_WARNINGS DO_PRAGMA(GCC diagnostic pop)
 #else
 #define DISABLE_ALL_WARNINGS
 #define RESTORE_WARNINGS
