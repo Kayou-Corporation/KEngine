add6b9820568091bf1586d577f779ad8e3713a3c
Corentin Bois Cotte <lasbiquettas@2a01cb0011c5e00016f389fdb09b00b4.ipv6.abo.wanadoo.fr>
2026-03-17 19:35:31 +0100

    [FIX] Errors with GNU cross compiler

--- Engine/Code/Core/Include/Utils/Core.hpp
+++ Engine/Code/Core/Include/Utils/Core.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #define KAYOU_GLOBAL Kayou
 
 #define BEGIN_NAMESPACE_KAYOU namespace KAYOU_GLOBAL {
@@ -10,16 +12,29 @@
 #define BEGIN_NAMESPACE_CORE BEGIN_NAMESPACE_KAYOU namespace CORE_NAMESPACE {
 #define END_NAMESPACE_CORE } /* namespace CORE_NAMESPACE */ END_NAMESPACE_KAYOU
 
-#if defined(__GNUC__) || defined(__clang__)
+
+#if defined(__GNUC__)
 #define DO_PRAGMA(x) _Pragma(#x)
 // Some warning flags might need to be added, see the flag in the output to add it here
 #define DISABLE_ALL_WARNINGS \
-        DO_PRAGMA(GCC diagnostic push) \
-        DO_PRAGMA(GCC diagnostic ignored "-Wall") \
-        DO_PRAGMA(GCC diagnostic ignored "-Wextra") \
-        DO_PRAGMA(GCC diagnostic ignored "-Wpedantic") \
-        DO_PRAGMA(GCC diagnostic ignored "-Wnullability-completeness")
+DO_PRAGMA(GCC diagnostic push) \
+DO_PRAGMA(GCC diagnostic ignored "-Wall") \
+DO_PRAGMA(GCC diagnostic ignored "-Wextra") \
+DO_PRAGMA(GCC diagnostic ignored "-Wpedantic") \
+DO_PRAGMA(GCC diagnostic ignored "-Wunused-parameter")
+DO_PRAGMA(GCC diagnostic ignored "-Wunused-variable")
+DO_PRAGMA(GCC diagnostic ignored "-Wmissing-field-initializers")
 #define RESTORE_WARNINGS DO_PRAGMA(GCC diagnostic pop)
+#elif defined(__clang__)
+#define DO_PRAGMA(x) _Pragma(#x)
+// Some warning flags might need to be added, see the flag in the output to add it here
+#define DISABLE_ALL_WARNINGS \
+        DO_PRAGMA(clang diagnostic push) \
+        DO_PRAGMA(clang diagnostic ignored "-Wall") \
+        DO_PRAGMA(clang diagnostic ignored "-Wextra") \
+        DO_PRAGMA(clang diagnostic ignored "-Wpedantic") \
+        DO_PRAGMA(clang diagnostic ignored "-Wnullability-completeness")
+#define RESTORE_WARNINGS DO_PRAGMA(clang diagnostic pop)
 #elif defined(_MSC_VER)
 // It is not possible to disable all warnings for MSVC, so if the macro doesn't ignore a specific warning you want ignored, feel free to add the warning code below
 #define DISABLE_ALL_WARNINGS __pragma(warning(push)) __pragma(warning(disable: 4100 4189 4244 4267 4456 4700 4701 4703 4996 4324))
