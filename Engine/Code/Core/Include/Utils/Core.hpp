446feba2b7ce41442400929c28f64466a3423683
Corentin Bois Cotte <cboiscotte@persistant.fr>
2026-03-31 11:54:02 +0200

    [UPDATE] Merge clang and gcc disable warnings macros

--- Engine/Code/Core/Include/Utils/Core.hpp
+++ Engine/Code/Core/Include/Utils/Core.hpp
@@ -13,7 +13,7 @@
 #define END_NAMESPACE_CORE } /* namespace CORE_NAMESPACE */ END_NAMESPACE_KAYOU
 
 
-#if defined(__GNUC__)
+#if defined(__GNUC__) || defined(__clang__)
 #define DO_PRAGMA(x) _Pragma(#x)
 // Some warning flags might need to be added, see the flag in the output to add it here
 #define DISABLE_WARNINGS \
@@ -24,17 +24,8 @@
         DO_PRAGMA(GCC diagnostic ignored "-Wunused-parameter")
         DO_PRAGMA(GCC diagnostic ignored "-Wunused-variable")
         DO_PRAGMA(GCC diagnostic ignored "-Wmissing-field-initializers")
+        DO_PRAGMA(GCC diagnostic ignored "-Wnullability-completeness")
 #define RESTORE_WARNINGS DO_PRAGMA(GCC diagnostic pop)
-#elif defined(__clang__)
-#define DO_PRAGMA(x) _Pragma(#x)
-// Some warning flags might need to be added, see the flag in the output to add it here
-#define DISABLE_WARNINGS \
-        DO_PRAGMA(clang diagnostic push) \
-        DO_PRAGMA(clang diagnostic ignored "-Wall") \
-        DO_PRAGMA(clang diagnostic ignored "-Wextra") \
-        DO_PRAGMA(clang diagnostic ignored "-Wpedantic") \
-        DO_PRAGMA(clang diagnostic ignored "-Wnullability-completeness")
-#define RESTORE_WARNINGS DO_PRAGMA(clang diagnostic pop)
 #elif defined(_MSC_VER)
 // Some warning flags might need to be added, see the flag in the output to add it here
 #define DISABLE_WARNINGS __pragma(warning(push)) __pragma(warning(disable: 4100 4189 4244 4267 4456 4700 4701 4703 4996 4324))
