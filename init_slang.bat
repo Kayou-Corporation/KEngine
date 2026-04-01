a2775de790e61d3fcc1e58468f4c54faf138f257
Ethan Ruhlmann <ethanrlmn@gmail.com>
2026-04-01 20:28:41 +0200

    [ADD] init_slang.bat now automatically asks for admin rights + uses relative path and allow selection between vs2022 & vs2026

--- init_slang.bat
+++ init_slang.bat
@@ -1,5 +1,14 @@
 @echo off
 SETLOCAL
+
+:: Checking for admin rights
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting admin rights...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit
+)
+
 REM Stop on error
 set "ERRORLEVEL=0"
 
@@ -7,10 +16,25 @@ echo Updating submodules...
 git submodule update --init --recursive
 
 echo Entering Slang directory...
-cd Engine\Libs\Slang
+cd /d "%~dp0Engine\Libs\Slang"
+
+echo Please select a CMake Preset:
+echo 1 - vs2022
+echo 2 - vs2026
+
+choice /c 12 /n /m "Your Choice (1 or 2) : "
+
+if errorlevel 2 (
+    set "CMAKE_PRESET=vs2026"
+) else (
+    set "CMAKE_PRESET=vs2022"
+)
+
+echo Preset chosen : %CMAKE_PRESET%
+echo.
 
 echo Configuring Slang...
-cmake --preset vs2022
+cmake --preset %CMAKE_PRESET%
 
 echo Building Slang (release preset)...
 cmake --build --preset release
