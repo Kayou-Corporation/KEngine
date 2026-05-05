38bbf9bf55d3a1a30bc28e1349a4da1b233afba3
e.rose <e.rose@student.isartdigital.com>
2026-05-05 15:44:50 +0200

    [WIP/NO MERGE] Still weird issue

--- Engine/Code/RHI/Include/Public/PipelineCommon.hpp
+++ Engine/Code/RHI/Include/Public/PipelineCommon.hpp
@@ -73,12 +73,12 @@ public:
 	KENGINE_API Core::RefCountPtr<DescriptorSetLayout> GetLayout() { return m_layout; }
 
 	KENGINE_API void SetType(PipelineType type) { m_type = type; }
-	KENGINE_API void SetLayout(Core::RefCountPtr<DescriptorSetLayout> layout) { m_layout = layout; }
+	KENGINE_API void SetLayout(Core::RefCountPtr<PipelineLayout> layout) { m_layout = layout; }
 
 
 protected:
 	PipelineType m_type;
-	Core::RefCountPtr<DescriptorSetLayout> m_layout;
+	Core::RefCountPtr<PipelineLayout> m_layout;
 
 };
 
